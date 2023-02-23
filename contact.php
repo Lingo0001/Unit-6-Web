5fee9bf9ae465de02a502dbe60b633c56933117e
Lingo0001 <danielsollie1@gmail.com>
2023-02-23 09:42:56 +0000

    Started Contact Form

--- contact.php
+++ contact.php
@@ -1,9 +1,19 @@
-<html>
-    <body>
-        
-        Welcome <?php echo $_POST["name"]; ?><br>
-        Your email address is: <?php echo $_POST["email"]; ?>
+<?php
+    // Get data from form
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $to = "admin@BerimboloSecurity.co.uk";
+    $subject = "This is the subject line";
 
-    </body>
+    $txt = "Name = ". $name . "\r\n Email = " . $email . "\r\n Message = ". $message;
 
-</html>
+    $headers = "From noreply@demosite.com" . "\r\n" . "CC: somebodyelse@example.com";
+
+    if ($email != NULL) {
+        mail($to, $subject, $txt, $headers);
+    }
+
+    header("Location:contact.html");
+?>
